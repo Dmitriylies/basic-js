57a3e4b49c0ad972f6d569a51fab4a34a7ab69df
Dmitriy <dimalie@live.com>
2021-09-13 19:53:29 +0300

    feat: add solution for task sort by height

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -1,3 +1,4 @@
+'use strict';
 import { NotImplementedError } from '../extensions/index.js';
 
 /**
@@ -11,7 +12,27 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  //throw new NotImplementedError('Not implemented');
+  let res = [];
+
+  arr.forEach((element, index) => {
+    if (element === -1){
+      res.push(index);
+    }
+  });
+
+  arr.sort(function(a,b){ 
+    return a - b;
+  });
+
+  arr = arr.slice(res.length);
+  for(let i = 0; i <res.length;i++) {
+    arr.splice(res[i], 0, -1);
+  }
+  return arr;
+  //console.log(arr);
 }
+//sortByHeight([ -1, -1, -1, -1, -1]);
+
+
