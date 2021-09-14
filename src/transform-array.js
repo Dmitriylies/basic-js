d5c6d5fc316733c5d73395dc1e74bf3830508c06
Dmitriy <dimalie@live.com>
2021-09-14 13:58:51 +0300

    feat: add solution for task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,3 +1,4 @@
+'use strict';
 import { NotImplementedError } from '../extensions/index.js';
 
 /**
@@ -13,7 +14,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform( arr ) {
+  //throw new NotImplementedError('Not implemented');
+  
+  if (!Array.isArray(arr) || arr === null) {
+    throw Error ("'arr' parameter must be an instance of the Array!");
+  }
+  let res = arr.slice(0); 
+  const length = arr.length - 1;
+
+  function double(operator, i, len) {
+    if (i !== len) {
+      res[i] = res[operator];
+    } else {
+      delete res[i];
+      res = res.flat();
+    }
+  }
+
+  function discard(i1, i) {
+    delete res[i];
+       delete res[i1];
+       res = res.flat(); 
+  }
+    for (let i = 0; i < arr.length; i++) {
+      if (res[i] === '--double-next' ) {
+        double(i+1, i, length);
+      }
+      if (res[i] === '--double-prev' && i !== 0) {
+        double(i-1, i, 0);
+      }
+      if (res[i] === '--discard-prev') {   
+        discard(i-1, i);
+      }
+      if (res[i] === '--discard-next') {
+        discard(i+1, i);
+      }
+    }
+
+    res.forEach((elem, i) => {
+      if (elem === '--double-prev' || elem === '--discard-prev') {
+        delete res[i];
+        res = res.flat();
+      }
+    });
+    
+    return res;
 }
+//transform( [ '--double-prev', true ] ) ;
